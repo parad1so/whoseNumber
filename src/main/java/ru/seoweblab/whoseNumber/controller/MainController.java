72ca895400235346e1f9e88e235c3b7c1ab31bac
parad1so <job@seoweblab.ru>
2021-03-12 23:59:31 +0300

    add freemaker

--- src/main/java/ru/seoweblab/whoseNumber/controller/MainController.java
+++ src/main/java/ru/seoweblab/whoseNumber/controller/MainController.java
@@ -3,6 +3,7 @@ package ru.seoweblab.whoseNumber.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,10 +26,19 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String main(Map<String, Object> model){
+    public String main(@RequestParam (required = false) String filter, Model model){
         Iterable<Message> messages =  messageRepo.findAll();
 
-        model.put("messages", messages);
+        if(filter != null && !filter.isEmpty()){
+            messages = messageRepo.findByTag(filter);
+        }
+        else {
+            messages = messageRepo.findAll();
+        }
+
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
+
         return "main";
     }
 
@@ -49,20 +59,6 @@ public class MainController {
         return "main";
     }
 
-    @PostMapping("filter")
-    public String filter(@RequestParam String filter, Map<String, Object> model){
-        Iterable<Message> messages;
 
-        if(filter != null && !filter.isEmpty()){
-            messages = messageRepo.findByTag(filter);
-        }
-        else {
-            messages = messageRepo.findAll();
-        }
-
-        model.put("messages", messages);
-
-        return "main";
-    }
 
 }
